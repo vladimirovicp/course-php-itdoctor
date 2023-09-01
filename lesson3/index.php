f9f2aca471bb2d97a19e0b31787203c6d8a3c838
vladimirovicp <vladimirovicp70@gmail.com>
2023-09-01 11:28:34 +0400

    add: 003 омментарии

---
+++ lesson3/index.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Lesson 3 - Комментарии</title>
+</head>
+<body>
+
+<h1>Комментарии</h1>
+
+<!-- Комментарий html -->
+
+
+<!-- ctrl + /  вставка комментарий в IDE-->
+
+<?php
+    /*
+     Комментарий 1
+    Комментарий 2
+    Комментарий 3
+    Комментарий 4
+    Комментарий 5
+     */
+    echo "Строка 1";  //Просто текстовая строка
+    echo "Строка 2"; #вторая текстовая строка
+?>
+
+</body>
+</html>
